99540a09617305af5ab628cbc01764df86dc9f98
Matrix <188987196+matrixlolz@users.noreply.github.com>
2024-12-02 12:41:26 +0530

    Update c2.py

--- c2.py
+++ c2.py
@@ -84,7 +84,7 @@ def ascii_vro():
     clear()
 
 def si():
-    print('         \x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mZxC \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to ZxC PN! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: zxcr9999 \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1')
+    print('         \x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mMatrix \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to CDDoS PN! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: Matrixlolz \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1')
     print("")
 
 def tools():
@@ -214,7 +214,7 @@ def amp_games():
 
 
 def menu():
-    sys.stdout.write(f"         \x1b]2;ZxC C2 --> Stars: [{bots}] | Online Users: [1] | Methods: [25] | Bypass: [10] | Amps: [1]\x07")
+    sys.stdout.write(f"         \x1b]2;Matrix C2 --> Stars: [{bots}] | Online Users: [1] | Methods: [25] | Bypass: [10] | Amps: [1]\x07")
     clear()
     print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mZxC \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to CDDOS C2! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: matrixlolz \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1| Store: https://infernosmm.com/')
     print("")
@@ -237,7 +237,7 @@ def menu():
 def main():
     menu()
     while(True):
-        cnc = input('''\x1b[38;2;0;212;14m╔══[C2\x1b[38;2;0;186;45m@Z\x1b[38;2;0;150;88mx\x1b[38;2;0;113;133mC\x1b[38;2;0;49;147m]
+        cnc = input('''\x1b[38;2;0;212;14m╔══[M1\x1b[38;2;0;186;45m@C\x1b[38;2;0;150;88mx\x1b[38;2;0;113;133mD\x1b[38;2;0;49;147m]
 \x1b[38;2;0;212;14m╚\x1b[38;2;0;186;45m═\x1b[38;2;0;150;88m═\x1b[38;2;0;113;133m═\x1b[38;2;0;83;168m═\x1b[38;2;0;49;147m➤ \x1b[38;2;239;239;239m''')
         if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
             layer7()
